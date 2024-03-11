b4c38d0686f5421adcacb397f8f91e9cbd8fc35f
Zackoxifye <mikaelcampo@yahoo.com>
2024-03-10 18:46:18 -0700

    update

--- ApexSurvivor/src/MainPages/Homepage.jsx
+++ ApexSurvivor/src/MainPages/Homepage.jsx
@@ -13,8 +13,14 @@ export default function Homepage() {
           // value={searchQuery}
           // onChange={handleInputChange}
         />
+        <button className="searchButton">Search</button>
       </div>
       <h2 className="featuredItems">Featured Items</h2>
+      <div className="squareContainer">
+        <div className="square square1"></div>
+        <div className="square square2"></div>
+        <div className="square square3"></div>
+      </div>
     </div>
   );
 }
