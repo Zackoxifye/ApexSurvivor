b146fccd00049b910373f38d282c4b303e126f84
Zackoxifye <mikaelcampo@yahoo.com>
2024-03-11 17:45:17 -0700

    update

--- ApexSurvivor/src/App.jsx
+++ ApexSurvivor/src/App.jsx
@@ -4,9 +4,10 @@ import Homepage from "./MainPages/Homepage";
 import AllGames from "./Games/AllGames";
 import Discussions from "./MainPages/Discussions";
 import Images from "./MainPages/Images";
+import Videos from "./MainPages/Videos";
 import Register from "./MainPages/Register";
 import SignIn from "./MainPages/SignIn";
-import Videos from "./MainPages/Videos";
+import Account from "./MainPages/Account";
 
 function App() {
   return (
@@ -18,9 +19,10 @@ function App() {
           <Route path="/all-games" element={<AllGames />} />
           <Route path="/discussions" element={<Discussions />} />
           <Route path="/images" element={<Images />} />
+          <Route path="/videos" element={<Videos />} />
           <Route path="/register" element={<Register />} />
           <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/videos" element={<Videos />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </BrowserRouter>
     </>
--- ApexSurvivor/src/MainPages/Account.jsx
+++ ApexSurvivor/src/MainPages/Account.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Account() {
-  return <div>Account</div>;
+  return (
+    <div>
+      <h1 className="comingSoon">Coming Soon!</h1>
+    </div>
+  );
 }
