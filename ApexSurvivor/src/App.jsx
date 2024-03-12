9bb0ec786eb4f5d67f9632206fcc5c93ca91af92
Zackoxifye <mikaelcampo@yahoo.com>
2024-03-11 18:07:05 -0700

    update

--- ApexSurvivor/src/App.jsx
+++ ApexSurvivor/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 import Homepage from "./MainPages/Homepage";
-import AllGames from "./Games/AllGames";
+import AllGames from "./MainPages/AllGames";
 import Discussions from "./MainPages/Discussions";
 import Images from "./MainPages/Images";
 import Videos from "./MainPages/Videos";
@@ -16,12 +16,12 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/all-games" element={<AllGames />} />
+          <Route path="/allgames" element={<AllGames />} />
           <Route path="/discussions" element={<Discussions />} />
           <Route path="/images" element={<Images />} />
           <Route path="/videos" element={<Videos />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/signin" element={<SignIn />} />
           <Route path="/account" element={<Account />} />
         </Routes>
       </BrowserRouter>
--- ApexSurvivor/src/MainPages/SignIn.jsx
+++ ApexSurvivor/src/MainPages/SignIn.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      <h1>Sign in</h1>
+    </div>
+  );
 }
